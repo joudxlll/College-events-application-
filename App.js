2f4044399db753ed04a1319e07cb3b1ba35d27a1
joudxlll <114012668+joudxlll@users.noreply.github.com>
2023-10-04 20:05:01 +0300

    our project

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/homepage';
+import ProfilePage from './screens/profilepage';
+import MyEventPage from './screens/myeventpage';
 
-export default function App() {
+
+
+function MyEventStackPage() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MyEventStack.Navigator>
+      <MyEventStack.Screen name="My Events" component={MyEventPage} />
+    </MyEventStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ProfileStackPage() {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Profile Page" component={ProfilePage} />
+    </ProfileStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+const MyEventStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+
+
+function HomeStackPage() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+<NavigationContainer>
+
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="profile" component={ProfileStackPage} />
+        <Tab.Screen name="Home" component={HomeStackPage} />
+        <Tab.Screen name="My Events" component={MyEventStackPage} />
+      </Tab.Navigator>
+      
+</NavigationContainer>
+  );
+}
---
+++ screens/myeventpage.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function MyEventPage() {
+  return (
+    <View>
+    </View>
+  );
+}
+
+export default MyEventPage;
