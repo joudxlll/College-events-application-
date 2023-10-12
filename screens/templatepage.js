11c2ae3d55a71c8d0b42684ed5ba6cfd5878c1af
Joud <114012668+joudxlll@users.noreply.github.com>
2023-10-12 18:25:05 +0300

    Add files via upload

---
+++ screens/templatepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+function TemplatePage({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>hi</Text>
+
+    </View>
+  );
+}
+
+export default TemplatePage;
