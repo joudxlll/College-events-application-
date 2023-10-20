d865f808d744a2d6b8a1bc64f4cc0cfcc83eb1d8
Joud <114012668+joudxlll@users.noreply.github.com>
2023-10-20 12:01:35 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,18 +2,22 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import HomeScreen from './screens/homepage';
-import ProfilePage from './screens/ProfilePage';
+import ProfilePage from './screens/profilepage';
 import MyEventPage from './screens/myeventpage';
 import TemplatePage from './screens/templatepage';
+import JoinUsPage from './screens/joinus';
+import AboutUsPage from './screens/aboutus';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { AntDesign } from '@expo/vector-icons';
 import EventDescription from './screens/EventDescription';
 
-
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function MyTabs() {
   return (
@@ -39,28 +43,36 @@ function MyTabs() {
       })}
     >
       <Tab.Screen name="Profile" component={ProfilePage} />
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: true }} />
       <Tab.Screen name="My Events" component={MyEventPage} />
     </Tab.Navigator>
   );
 }
 
+function MyDrawer(){
+  return (
+    <Drawer.Navigator initialRouteName='Dashboard'>
+      <Drawer.Screen name="Home" component={MyTabs}  />
+      <Drawer.Screen name="Join Us" component={JoinUsPage} />
+      <Drawer.Screen name="About Us" component={AboutUsPage} />
+    </Drawer.Navigator>
+  )
+}
+
+
+
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="home" component={MyTabs} />
-
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfilePage} />
-        <Stack.Screen name="My Events" component={MyEventPage} />
-        <Stack.Screen name="Template" component={TemplatePage} />
-
-        <Stack.Screen name="EventDescription" component={EventDescription}
-          options={{ headerShown: true }}
-        />
-
+        <Stack.Screen name="Home" component={MyDrawer} />
+        <Stack.Screen name="Template" component={TemplatePage} options={{ headerShown: true }} />
+        <Stack.Screen name="EventDescription" component={EventDescription} options={{ headerShown: true }} />
+        <Stack.Screen name="About Us" component={AboutUsPage} options={{ headerShown: true }} />
+        <Stack.Screen name="Join Us" component={JoinUsPage} options={{ headerShown: true }} />
       </Stack.Navigator>
     </NavigationContainer>
+    
   );
 }
