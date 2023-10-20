ad07f24d8836e167749d0ce13b18077437c93599
Atheer31-3 <105993810+Atheer31-3@users.noreply.github.com>
2023-10-20 20:16:22 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,10 @@
 export const categories = [
     'Educatin',
     'Ux/Ui',
-    'Scrurty',
-    'AI'
+    'Security ',
+    'Artificial intelligence',
+    'Gaming'
+    
 ]
 export const nameRequest= [
     'AllRequest',
@@ -16,7 +18,7 @@ export const eventData = [
         title: 'Learn Python',
         description: "Join us for an interactive session where we dive into the fundamentals of Python programming. Whether you're a beginner or looking to brush up on your skills, this event is perfect for you. Get ready to explore the syntax, data types, and control structures of Python in a fun and engaging way. Don't miss this opportunity to level up your coding game! #Python #CodingEvent",
 
-        date: '2023-10-01',
+        date: '2023-05-08',
         time: '10:00 AM',
         location: 'hall 1',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/121px-Python-logo-notext.svg.png',
@@ -24,35 +26,56 @@ export const eventData = [
     },
     {
         id: 2,
-        title: 'Event Learn Javascript',
+        title: 'Learn Javascript',
         description: "Calling all aspiring web developers! Join us for an exciting session where we unravel the mysteries of JavaScript. Whether you're new to programming or want to enhance your JavaScript skills, this event is perfect for you. Discover the power of JavaScript as we delve into variables, functions, loops, and more. Get ready to build interactive web applications and bring your ideas to life! Don't miss out on this opportunity to level up your coding skills. #JavaScript #WebDevelopment #CodingEvent",
         date: '2023-05-02',
         time: '11:00 AM',
         location: 'Location 2',
         photo: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F6a%2FJavaScript-logo.png&f=1&nofb=1&ipt=fc8ff21b83c4c2029d71fcbc98af377ba05a1382081a0c672d6a88e955cacedd&ipo=images',
-        category: categories[1],
+        category: categories[0],
     },
     {
         id: 3,
-        title: 'Event 3',
+        title: 'ux/ui',
         description: 'Description 3',
         date: '2023-05-03',
         time: '12:00 PM',
         location: 'Location 3',
-        photo: 'https://picsum.photos/700',
-        category: categories[2],
+        photo: 'https://community.nasscom.in/sites/default/files/styles/960_x_600/public/media/images/What%20is%20UIUX%20Transformation%20and%20How%20Does%20it%20Benefit%20Businesses-04_0.jpg?itok=nz_CcG8G',
+        category: categories[1],
     },
     {
         id: 4,
-        title: 'Event 4',
+        title: 'Artificial intelligence',
         description: 'Description 4',
-        date: '2023-05-04',
+        date: '2023-05-20',
         time: '01:00 PM',
         location: 'Location 4',
-        photo: 'https://picsum.photos/400',
+        photo: 'https://media.licdn.com/dms/image/D5612AQFYPqHE53ERCA/article-cover_image-shrink_600_2000/0/1686036335375?e=2147483647&v=beta&t=TWbyjE1gKiiG4Z80cirbDccvdDHFVoKb7yCpj6LY34c',
+        category: categories[3],
+    },
+    {
+        id: 5,
+        title: 'Security ',
+        description: 'Description 5',
+        date: '2023-06-04',
+        time: '01:00 PM',
+        location: 'Location 5',
+        photo: 'https://www.thestreet.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTg4MjMxMjQyNjIwMDg2MTY1/security-top-image.webp',
         category: categories[2],
+    },
+    {
+        id: 6,
+        title: 'Gaming ',
+        description: 'Description 6',
+        date: '2023-12-10',
+        time: '01:00 PM',
+        location: 'Location 6',
+        photo: 'https://www.roadlesstraveledstore.com/wp-content/uploads/2021/10/game-based-learning-1.png',
+        category: categories[4],
     }
 ]
+
 export const eventrequest =[
     {
         id: 1,
@@ -60,47 +83,45 @@ export const eventrequest =[
         title: 'Learn Python',
         description: "Join us for an interactive session where we dive into the fundamentals of Python programming. Whether you're a beginner or looking to brush up on your skills, this event is perfect for you. Get ready to explore the syntax, data types, and control structures of Python in a fun and engaging way. Don't miss this opportunity to level up your coding game! #Python #CodingEvent",
 
-        date: '2023-10-01',
+        date: '2023-05-08',
         time: '10:00 AM',
         location: 'hall 1',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/121px-Python-logo-notext.svg.png',
-        nameRequest: nameRequest[1],
+        category: categories[0],
     },
     {
         id: 2,
         user:"nada",
-        title: 'Event Learn Javascript',
+        title: 'Learn Javascript',
         description: "Calling all aspiring web developers! Join us for an exciting session where we unravel the mysteries of JavaScript. Whether you're new to programming or want to enhance your JavaScript skills, this event is perfect for you. Discover the power of JavaScript as we delve into variables, functions, loops, and more. Get ready to build interactive web applications and bring your ideas to life! Don't miss out on this opportunity to level up your coding skills. #JavaScript #WebDevelopment #CodingEvent",
         date: '2023-05-02',
         time: '11:00 AM',
         location: 'Location 2',
         photo: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F6a%2FJavaScript-logo.png&f=1&nofb=1&ipt=fc8ff21b83c4c2029d71fcbc98af377ba05a1382081a0c672d6a88e955cacedd&ipo=images',
-        nameRequest: nameRequest[1],
+        category: categories[0],
     },
     {
         id: 3,
         user: "Atheer",
-        title: 'jave',
+        title: 'ux/ui',
         description: 'Description 3',
         date: '2023-05-03',
         time: '12:00 PM',
         location: 'Location 3',
-        photo: 'https://picsum.photos/700',
-        nameRequest: nameRequest[2],
-
+        photo: 'https://community.nasscom.in/sites/default/files/styles/960_x_600/public/media/images/What%20is%20UIUX%20Transformation%20and%20How%20Does%20it%20Benefit%20Businesses-04_0.jpg?itok=nz_CcG8G',
+        category: categories[1],
     },
     {
         id: 4,
         user:"shaden",
-        title: 'Ux/Ui',
+        title: 'Artificial intelligence',
         description: 'Description 4',
-        date: '2023-05-04',
+        date: '2023-05-20',
         time: '01:00 PM',
         location: 'Location 4',
-        photo: 'https://picsum.photos/400',
-        nameRequest: nameRequest[2],
-    }
-
+        photo: 'https://media.licdn.com/dms/image/D5612AQFYPqHE53ERCA/article-cover_image-shrink_600_2000/0/1686036335375?e=2147483647&v=beta&t=TWbyjE1gKiiG4Z80cirbDccvdDHFVoKb7yCpj6LY34c',
+        category: categories[2],
+    },
 ]
 
 const users=[
