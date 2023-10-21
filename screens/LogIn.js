0b8091be9b303bea761ebfe91549f9156dd3b472
Nada Alkharji <129560713+NadaAbdulmalek@users.noreply.github.com>
2023-10-21 23:34:29 +0300

    Update LogIn.js

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
  
 
@@ -7,7 +7,7 @@ const LogIn = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
- 
+
 
   const handleLogin = () => {
     
@@ -21,30 +21,55 @@ const LogIn = ({ navigation }) => {
     navigation.navigate('ResetPassword');
   };
 
- 
+
 
   return (
-<View>
-<TextInput
+    <View style ={styles.container}>
+     <Text>E-mail</Text>
+     <TextInput
+        style ={styles.input}
         placeholder="E-mail"
         value={email}
-        onChangeText={setEmail}
-      />
-<TextInput
+        onChangeText={setEmail} />
+
+
+     <Text>Password</Text>
+     <TextInput
+        style ={styles.input}
         placeholder="Password"
         secureTextEntry
         value={password}
-        onChangeText={setPassword}
-      />
+        onChangeText={setPassword} />
+
+
+
+
 <Button title="Log in" onPress={handleLogin} />
 
  
 
       <TouchableOpacity onPress={handleForgotPassword}>
-<Text>Forgot your password?</Text>
-</TouchableOpacity>
+        <Text>Forgot your password?</Text>
+      </TouchableOpacity>
 </View>
   );
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#777',
+      padding: 8,
+      margin: 10,
+      width: 200,
+    }
+
+  })
 };
 
 
